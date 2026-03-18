4921a26f23c253993938d02d0c2144680e748dc5
wertyiu28 <wertyiu28@naver.com>
2026-03-18 10:49:20 +0900

    위험지수 분석 그래프 분할

--- app/src/faq.py
+++ app/src/faq.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 def render():
-    st.title("🙋 FAQ")
+    st.title("FAQ")
     with st.expander("데이터 정렬 기준은 무엇인가요?"):
         st.write("메인 페이지 차트는 사고 발생 건수가 적은 지역부터 오름차순으로 나열되어 있습니다.")
