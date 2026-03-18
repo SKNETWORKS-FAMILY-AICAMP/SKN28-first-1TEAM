079e1e1b3732ddd09634654748b5f08194f27aac
WHwi99 <minuk990965@gmail.com>
2026-03-18 09:19:57 +0900

    미사용 파일 제거, 내용 수정

--- app/src/danger.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import plotly.express as px
-
-def render(dp):
-    st.title("🧪 인구/차량 대비 사고 위험도 분석")
-    df = dp.get_danger_index()
-    df['danger_score'] = (df['total_accidents'] / df['population_count']) * 10000
-    fig = px.scatter(df, x="car_count", y="total_accidents", size="danger_score", color="gu", hover_name="gu")
-    st.plotly_chart(fig, use_container_width=True)
